4089a375c8027b85c4bc98bea4d6bc864ff9a492
Gael Gohoungo <gohoungogael@gmail.com>
2019-10-12 11:50:08 -0400

    modified ligic.js

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,8 +1,14 @@
-
-
-$(document).ready(function () {
-    
-    // Get Location 
+$(document).ready(function (response) {
+    var APIKey = "032d25d4514ca8a1b170b39afbfd4f50";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+    "q=curent city=.............=" + APIKey;
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    var response = {};
+    // .then (function(response){
+ // Get Location 
     navigator.geolocation.getCurrentPosition(success, error);
 
     function success(pos) {
@@ -26,72 +32,38 @@ $(document).ready(function () {
 
     // Update Dom
     function updateDOM(data) {
-        var city = data.name;
+        var curent = data.name;
         var temp = Math.round(data.main.temp_max);
         var desc = data.weather[0].description;
         var icon = data.weather[0].icon;
 
-        $('#city').html(city);
-        $('#temp').html(temp);
+        $('#current').html(current);
+        $('#temp').html(temp)
         $('#desc').html(desc);
         $('#icon').attr('src', icon);
     }
 });
-
-
-*/////////////////////////////////////////
-
-
-var weatherCondition = [
-
-];
-
-for var i = 0; i < currentWeather.lenght; i++ {
-
-}
-if (weatherCondition < 0 && ) {
- ................ ;
-} else if (weatherCondition < 30) {
-  ...............;
-} else if {weatherCondition < 60
-  ...............;
-} else if {weatherCondition < 100
-  .................;
-} else {
-  ................;
-}
-
-var APIKey = "032d25d4514ca8a1b170b39afbfd4f50";
-
-    
-    var queryURL = "https://openweathermap.org/find?q=Atlanta"+ APIKey;
-
-    
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-1-create a web app that plays music according to the weather
-2-API calls from music app and and weather app
-   *Call weather API First (Time, temperature, rain, sun, snow...)
-   *Call Music API then (cool Jazz, rock, funck, slow, R&B...)
-3-create variable that hold the current weather
-4-Loop through that variable to get the current weather 
-5- if condition that determine the current weather 
-6-Display music according to the weather
-*/
+ // Transfer content to HTML
+ $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+ $(".wind").text("Wind Speed: " + response.wind.speed);
+ $(".humidity").text("Humidity: " + response.main.humidity);
+ $(".temp").text("Temperature (F) " + response.main.temp);
+// if (weatherCondition < 0 && ) {
+//  ................ ;
+// } else if (weatherCondition < 30) {
+//   ...............;
+// } else if {weatherCondition < 60
+//   ...............;
+// } else if {weatherCondition < 100
+//   .................;
+// } else {
+//   ................;
+// }
+// 1-create a web app that plays music according to the weather
+// 2-API calls from music app and and weather app
+//    *Call weather API First (Time, temperature, rain, sun, snow...)
+//    *Call Music API then (cool Jazz, rock, funck, slow, R&B...)
+// 3-create variable that hold the current weather
+// 4-Loop through that variable to get the current weather 
+// 5- if condition that determine the current weather 
+// Display music according to the weather
